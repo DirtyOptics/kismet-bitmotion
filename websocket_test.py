69389fc815af1ebf7e9129600e315fc0eff1eb72
DirtyOptics <dblack@dirtyoptics.com>
2024-08-18 16:14:30 +0800

    Update websocket_test.py

--- websocket_test.py
+++ websocket_test.py
@@ -3,22 +3,19 @@ import websockets
 import json
 import logging
 
-# Enable debug logging
-logging.basicConfig(level=logging.DEBUG)
+# Enable debug logging and set up logging to a file
+logging.basicConfig(level=logging.DEBUG, filename='websocket_output.txt', filemode='w', format='%(message)s')
 
 async def listen_to_kismet():
-    websocket_url = 'ws://localhost:2501/eventbus/events.ws?KISMET=D470660EF5CB6466F4B8143B204F8816'  # Replace with your WebSocket URL
+    websocket_url = 'ws://localhost:2501/eventbus/events.ws?KISMET=D470660EF5CB6466F4B8143B204F8816'
 
     async with websockets.connect(websocket_url) as websocket:
-        # Subscribe to the DOT11_ADVERTISED_SSID event
         subscribe_message = {
             "SUBSCRIBE": "DOT11_ADVERTISED_SSID"
         }
         await websocket.send(json.dumps(subscribe_message))
-        print("Subscribed to DOT11_ADVERTISED_SSID event")
 
-        while True:
-            message = await websocket.recv()
-            print("Received:", message)
+        async for message in websocket:
+            logging.info(message)
 
 asyncio.run(listen_to_kismet())
