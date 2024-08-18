1fe38810e2d2b3aa5d3d74abe214e2467328e211
DirtyOptics <dblack@dirtyoptics.com>
2024-08-18 15:31:34 +0800

    Update websocket_test.py

--- websocket_test.py
+++ websocket_test.py
@@ -6,7 +6,7 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 
 async def listen_to_kismet():
-    websocket_url = 'ws://localhost:2501/eventbus/events.ws?KISMET=<D470660EF5CB6466F4B8143B204F8816>'  # Replace with your WebSocket URL
+    websocket_url = 'ws://localhost:2501/eventbus/events.ws?KISMET=D470660EF5CB6466F4B8143B204F8816'  # Replace with your WebSocket URL
 
     async with websockets.connect(websocket_url) as websocket:
         while True:
