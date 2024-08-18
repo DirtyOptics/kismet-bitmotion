1e368928326b353ab3601e5b34b7355a6f8fa72d
DirtyOptics <dblack@dirtyoptics.com>
2024-08-18 14:38:05 +0800

    Update websocket_test.py

--- websocket_test.py
+++ websocket_test.py
@@ -2,6 +2,9 @@ import asyncio
 import websockets
 import json
 
+# To track updates to a particular AP
+tracked_aps = {}
+
 async def test_websocket():
     uri = "ws://localhost:2501/eventbus/events.ws?KISMET=D470660EF5CB6466F4B8143B204F8816"
     async with websockets.connect(uri) as websocket:
@@ -9,11 +12,31 @@ async def test_websocket():
         subscribe_message = {"SUBSCRIBE": "DOT11_ADVERTISED_SSID"}
         await websocket.send(json.dumps(subscribe_message))
 
-        # Receive and print messages
         while True:
             try:
                 message = await websocket.recv()
-                print(message)
+                data = json.loads(message)
+
+                # Look for DOT11_ADVERTISED_SSID data
+                if "DOT11_ADVERTISED_SSID" in data:
+                    ssid_data = data["DOT11_ADVERTISED_SSID"]
+                    bssid = ssid_data.get("dot11.advertisedssid.bssid", None)
+                    ssid = ssid_data.get("dot11.advertisedssid.ssid", "(hidden)")
+                    signal = ssid_data.get("dot11.advertisedssid.signal_dbm", None)
+                    
+                    if bssid:
+                        # Check if this AP has been seen before
+                        if bssid in tracked_aps:
+                            previous_signal = tracked_aps[bssid]['signal']
+                            # If the signal has changed, print the update
+                            if signal != previous_signal:
+                                print(f"AP {bssid} ({ssid}) signal changed from {previous_signal} to {signal}")
+                                tracked_aps[bssid]['signal'] = signal
+                        else:
+                            # First time seeing this AP
+                            tracked_aps[bssid] = {'ssid': ssid, 'signal': signal}
+                            print(f"First seen AP {bssid} ({ssid}) with signal {signal}")
+                    
             except websockets.exceptions.ConnectionClosed:
                 print("Connection closed")
                 break
